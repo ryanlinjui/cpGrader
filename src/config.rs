bd7de00aacbf1c93974cc649f514cbdc28c9c27c
ycchung <ycchung@g36-macbook-pro.taile9128.ts.net>
2024-08-28 11:12:07 +0800

    init:first

---
+++ src/config.rs
@@ -0,0 +1,12 @@
+// config.rs
+
+pub const DEFAULT_WORKSPACE_DIR: &str = ".";
+pub const DEFAULT_TESTCASE_SRC: &str = "testcase";
+pub const DEFAULT_GRADING_REPORT_PATH: &str = "report.csv";
+pub const DEFAULT_EXTRACTED_FILES_DIR: &str = "files";
+pub const DEFAULT_CONFIG_FILE: &str = "config.toml";
+pub const DEFAULT_CASE_NAME: &str = "DefaultCase";
+
+pub const ALL_MATCH_CASE: u8 = 5 * 1024 * 1024 ;
+
+pub const ALL_MATCH_CASE: &str = "*";
---
+++ src/main.rs
@@ -0,0 +1,9 @@
+// main.rs
+mod config;
+use config::*;
+
+fn main() {
+    println!("Hello, world!");
+    println!("Workspace dir: {}", DEFAULT_WORKSPACE_DIR);
+    // 這裡可以使用其他常量
+}
